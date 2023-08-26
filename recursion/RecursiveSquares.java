91ca8db6bc4ec1f4b1e68580e172a5f0980b763d
Jing - Peters MBP <jingcao.me@gmail.com>
2023-08-26 18:33:41 -0400

    add recursion exercises

--- recursion/RecursiveSquares.java
+++ recursion/RecursiveSquares.java
@@ -1,3 +1,41 @@
 public class RecursiveSquares {
-	
+
+
+    // Draws a square centered on (x, y) of the given side length
+    // with a light gray background and a black border.
+    public static void drawSquare(double x, double y, double halfLen){
+    	
+    	StdDraw.setPenColor(StdDraw.BLACK);  // black 
+    	StdDraw.square(x, y, halfLen);  // border
+    	
+    	StdDraw.setPenColor(StdDraw.LIGHT_GRAY);  // light gray 
+    	StdDraw.filledSquare(x, y, halfLen);  // background
+    }
+
+
+    // Draws a recursive square pattern of order n, centered on (x, y)
+    // of the given side length.
+    public static void draw(int n, double x, double y, double len){
+
+    	double halfLen = 0.5 * len;
+    	drawSquare (x, y, halfLen);
+
+    	// base case
+    	if (n == 1) return;	
+
+    	// recursively draw 4 more squares
+    	draw(n - 1, x + halfLen, y + halfLen, halfLen);
+    	draw(n - 1, x + halfLen, y - halfLen, halfLen);
+    	draw(n - 1, x - halfLen, y + halfLen, halfLen);
+    	draw(n - 1, x - halfLen, y - halfLen, halfLen);
+    }
+
+    // Takes an integer command-line argument n and draws a recursive square 
+    // pattern of order n, centered on (0.5, 0.5), with side length 0.5.
+    public static void main(String[] args){
+
+    	int n = Integer.parseInt(args[0]);
+    	draw(n, 0.5, 0.5, 0.5);
+
+    }
 }
--- recursion/RevesPuzzle.java
+++ recursion/RevesPuzzle.java
@@ -1,52 +1,32 @@
 public class RevesPuzzle {
 
-    public static int getK(int n) {
-        return n + 1 - (int)(Math.sqrt(2 * n + 1));
-    }
-
-        // if (n == 0) return "/n";
-        // String move;
-        // if (left) move = n + "L";
-        // else move = n + "R";
-        // return hanoi(n - 1, !left) + move + hanoi(n - 1, !left);
-
-    public static String hanoi(int n, char start, char dest, char spare) {
-        String move = "";
-        if (n == 1) {
-            move = "Move disc " + n + " from " + start + " to " + dest;
-        }
-        return move;
+    private static String hanoi(int k, int n, char start, char dest, char spare) {
         
-    }
-
-    public static String reves(int n) {
-
-        char[] poles = new char[]{'A', 'B', 'C', 'D'};
-        // base case:
-        // if (n == 0) return "\n";
-
-        // recursive case
-        char start = poles[0];
-        char dest = poles[3];
-        char spare = poles[2];
-
-        // int k = (int) Math.round(n + 1 - (Math.sqrt(2 * n + 1)));
-
-        // recursively transfer k smallest discs to single pole other than start or dest poles
-
-        // recursively transfer remaining n-k disks to destination pole, without using the pole used above, using hanoi
-        String move = hanoi(n, start, dest, spare);
+        // base case
+        if (k == n) return ("Move disc " + n + " from " + start + " to " + dest + "\n");
         
-        // recursively transfer k smallest discs to destination pole
-
-
-        return move;
+        // recursive
+        return hanoi(k, n-1, start, spare, dest) + ("Move disc " + n + " from " + start + " to " + dest + "\n") + hanoi(k, n-1, spare, dest, start);        
     }
+
+    private static String reves(int n, char start, char dest, char spare1, char spare2) {
+
+        int k = (int) Math.round(n + 1 - (Math.sqrt(2 * n + 1)));
+        
+        // base case
+        if (n == 1) return ("Move disc " + n + " from " + start + " to " + dest + "\n");
+
+        // recursive
+        return reves(k, start, spare1, spare2, dest) + hanoi(k+1, n, start, dest, spare2) + reves(k, spare1, dest, spare2, start);
+
+    }
+
 
     public static void main(String[] args) {
+
         int n = Integer.parseInt(args[0]);
-        // StdOut.println(getK(n));
-        StdOut.println(reves(n));
+        StdOut.println(reves(n, 'A', 'D', 'B', 'C'));
+
     }
 
 }
